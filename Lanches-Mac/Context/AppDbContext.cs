ae2d0db95f498f29c01dc86b69b01fa27944fab2
Rafael Borges <borgesrafael.ti@gmail.com>
2021-02-01 12:54:53 -0300

    Configuração do Entity Framework Core

---
+++ Lanches-Mac/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lanches_Mac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches_Mac.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
