a5f96662c8812d5fbdaa3532d1edd2758e3ba244
Rafael Borges <borgesrafael.ti@gmail.com>
2021-02-01 23:08:22 -0300

    Configura o modelo de domínio com DataAnnotations

--- Lanches-Mac/Models/Categoria.cs
+++ Lanches-Mac/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace Lanches_Mac.Models
     public class Categoria
     {
         public int CategoriaId { get; set; }
+        [StringLength(100)]
         public string CategoriaNome { get; set; }
+        [StringLength(200)]
         public string Descricao { get; set; }
         public List<Lanche> Lanches { get; set; }
     }
--- Lanches-Mac/Models/Lanche.cs
+++ Lanches-Mac/Models/Lanche.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +11,17 @@ namespace Lanches_Mac.Models
     public class Lanche
     {
         public int LancheId { get; set; }
+        [StringLength(100)]
         public string Nome { get; set; }
+        [StringLength(100)]
         public string DescricaoCurta { get; set; }
+        [StringLength(255)]
         public string DescricaoDetalhada { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Preco { get; set; }
+        [StringLength(200)]
         public string ImagemUrl { get; set; }
+        [StringLength(200)]
         public string ImagemThumbnailUrl { get; set; }
         public bool IsLanchePreferido { get; set; }
         public bool EmEstoque { get; set; }
