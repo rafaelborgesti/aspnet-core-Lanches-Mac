5c2fec8d6a690a679c1036c36b65e0ba259ecd23
Rafael Borges <borgesrafael.ti@gmail.com>
2021-02-07 23:49:57 -0300

    Listagem de lanches

---
+++ Lanches-Mac/Controllers/LancheController.cs
@@ -0,0 +1,27 @@
+﻿using Lanches_Mac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches_Mac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, 
+            ICategoriaRepository categoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categoria";
+
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
